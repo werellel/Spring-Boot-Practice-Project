255f8d8ba7b479fd1082d3e9e43c0be002d81675
werellel <werellel@gmail.com>
2020-12-18 22:15:59 +0900

    Add update and delete

--- Board/src/main/java/board/mapper/BoardMapper.java
+++ Board/src/main/java/board/mapper/BoardMapper.java
@@ -20,4 +20,8 @@ public interface BoardMapper {
 	void updateHitCount(int boardIdx) throws Exception;
 	
 	BoardDto selectBoardDetail(int boardIdx) throws Exception;
+	
+	void updateBoard(BoardDto board) throws Exception;
+	
+	void deleteBoard(int boardIdx) throws Exception;
 }
--- Board/src/main/java/board/service/BoardService.java
+++ Board/src/main/java/board/service/BoardService.java
@@ -12,4 +12,8 @@ public interface BoardService {
 	void insertBoard(BoardDto board) throws Exception;
 	
 	BoardDto selectBoardDetail(int boardIdx) throws Exception;
+	
+	void updateBoard(BoardDto board) throws Exception;
+	
+	void deleteBoard(int boardIdx) throws Exception;
 }
