d25029161bee5df136eb7f4f35b8243307befe38
werellel <werellel@gmail.com>
2020-12-21 01:27:35 +0900

    Add setting for character encoding

--- Board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ Board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,8 +1,14 @@
 package board.configuration;
 
+import java.nio.charset.Charset;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import board.interceptor.LoggerInterceptor;
 
@@ -16,5 +22,32 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 		제외할 요청 주소의 패턴을 지정하여 선택적으로 적용할 수 있다.
 		*/		
 		registry.addInterceptor(new LoggerInterceptor());
+	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		/*
+		CharacterEncodingFilter은 스프링이 제공하는 클래스로 웹에서 주고받는 데이터의 헤더값을 UTF-8로 인코딩해 준다.
+		*/	
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		/*
+		 기본값은 false이다. 기본값으로 설정되어 있으면 HttpServletRequest의 getCharacterEncoding 메서드의
+		 반환값이 null이 아닌 경우에만 인코딩을 변경하지 않는다. null이 아니라는 것은 특정한 인코딩이 설정되어 있다는 의미다.
+		 즉, 특정한 인코딩이 지정되지 않을 때만 인코딩을 변경한다.
+		 반대로 null인 경우에는 설정된 인코딩으로 변경한다.  
+		 forceEncoding이 true이면
+		 입력값(HttpServletRequest)과 결과값(HttpServeltResponse)
+		 이 강제적으로 설정된 인코딩으로 변경한다. 즉 HttpServletRequest에 특정한 인코딩이 설정되어
+		 있는 경우에는 인코딩을 변경하지 않고, 설정되어 있지 않은 경우에만 지정한 인코딩으로 변경한다.
+		*/
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return characterEncodingFilter;
+	}
+	
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter(){
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
 	}
 }
