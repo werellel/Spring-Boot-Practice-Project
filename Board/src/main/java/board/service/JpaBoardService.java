d67c41206c14a033f4b4b60b7de57f98108adfda
werellel <werellel@gmail.com>
2021-01-02 23:47:38 +0900

    Add jpa service

---
+++ Board/src/main/java/board/service/JpaBoardService.java
@@ -0,0 +1,21 @@
+package board.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import board.entity.BoardEntity;
+import board.entity.BoardFileEntity;
+
+public interface JpaBoardService {
+
+	List<BoardEntity> selectBoardList() throws Exception;
+
+	void saveBoard(BoardEntity board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception;
+	
+	BoardEntity selectBoardDetail(int boardIdx) throws Exception;
+
+	void deleteBoard(int boardIdx);
+
+	BoardFileEntity selectBoardFileInformation(int boardIdx, int idx) throws Exception;
+}
